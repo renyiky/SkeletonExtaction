a2f300b361524417b7235f422987b51d61036594
Ren Yi <renyiky@qq.com>
2020-07-03 14:57:12 +0800

    change rectangle searching to circle searching

--- src/getD3nn.cpp
+++ src/getD3nn.cpp
@@ -17,47 +17,47 @@ double getD3nn(Mat &img, const struct skelx::Point &point){
     vector<double> neighborsRadius = {};
 
     // rectangle search
-    while(neighborsRadius.size() < 3){
-        ++radius;
-        for(int i = -radius; i < radius + 1; ++i){
-            if(i == -radius || i == radius){
-                for(int j = -radius; j < radius + 1; ++j){
-                    if(x + i >= 0 && x + i < rows && y + j >= 0 
-                        && y + j < cols && img.at<uchar>(x + i, y + j) != 0 
-                        && !(i == 0 && j == 0)){
-                        neighborsRadius.push_back(static_cast<double>(radius));
-                    }
-                }
-            }else{
-                int j = -radius;
-                if(x + i >= 0 && x + i < rows && y + j >= 0 
-                    && y + j < cols && img.at<uchar>(x + i, y + j) != 0 
-                    && !(i == 0 && j == 0)){
-                    neighborsRadius.push_back(static_cast<double>(radius));
-                }
-
-                j = radius;
-                if(x + i >= 0 && x + i < rows && y + j >= 0 
-                    && y + j < cols && img.at<uchar>(x + i, y + j) != 0 
-                    && !(i == 0 && j == 0)){
-                    neighborsRadius.push_back(static_cast<double>(radius));
-                }
-            }
-        }
-    }
-
-    // circle search
-    // while(neighborsRadius.size() < 4){
+    // while(neighborsRadius.size() < 3){
     //     ++radius;
-    //     neighborsRadius = {};
-    //     for(double i = -radius; i < radius + 1; ++i){
-    //         for(double j = -radius; j < radius + 1; ++j){
-    //             if(pow((i * i + j * j), 0.5) <= radius && x + i >= 0 && x + i < img.rows && y + j >= 0 && y + j < img.cols && img.at<uchar>(x + i, y + j) != 0 && !(i == 0 && j == 0)){
-    //                 neighborsRadius.push_back(pow((i * i + j * j), 0.5));
+    //     for(int i = -radius; i < radius + 1; ++i){
+    //         if(i == -radius || i == radius){
+    //             for(int j = -radius; j < radius + 1; ++j){
+    //                 if(x + i >= 0 && x + i < rows && y + j >= 0 
+    //                     && y + j < cols && img.at<uchar>(x + i, y + j) != 0 
+    //                     && !(i == 0 && j == 0)){
+    //                     neighborsRadius.push_back(static_cast<double>(radius));
+    //                 }
+    //             }
+    //         }else{
+    //             int j = -radius;
+    //             if(x + i >= 0 && x + i < rows && y + j >= 0 
+    //                 && y + j < cols && img.at<uchar>(x + i, y + j) != 0 
+    //                 && !(i == 0 && j == 0)){
+    //                 neighborsRadius.push_back(static_cast<double>(radius));
+    //             }
+
+    //             j = radius;
+    //             if(x + i >= 0 && x + i < rows && y + j >= 0 
+    //                 && y + j < cols && img.at<uchar>(x + i, y + j) != 0 
+    //                 && !(i == 0 && j == 0)){
+    //                 neighborsRadius.push_back(static_cast<double>(radius));
     //             }
     //         }
     //     }
     // }
-    // sort(neighborsRadius.begin(), neighborsRadius.end());
+
+    // circle search
+    while(neighborsRadius.size() < 3){
+        ++radius;
+        neighborsRadius = {};
+        for(double i = -radius; i < radius + 1; ++i){
+            for(double j = -radius; j < radius + 1; ++j){
+                if(pow((i * i + j * j), 0.5) <= radius && x + i >= 0 && x + i < img.rows && y + j >= 0 && y + j < img.cols && img.at<uchar>(x + i, y + j) != 0 && !(i == 0 && j == 0)){
+                    neighborsRadius.push_back(pow((i * i + j * j), 0.5));
+                }
+            }
+        }
+    }
+    sort(neighborsRadius.begin(), neighborsRadius.end());
     return (neighborsRadius[0] + neighborsRadius[1] + neighborsRadius[2]) / 3;   // /3
 }
