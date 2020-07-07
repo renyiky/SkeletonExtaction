241fa2e982f2a33b937e6f9fb513125bd66de910
Ren Yi <renyiky@qq.com>
2020-07-07 17:00:04 +0800

    add data generator, it includes raw and ground truth which are manually selected

---
+++ dataGenerator/pruned/t.cpp
@@ -0,0 +1,21 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <string>
+
+using namespace std;
+using namespace cv;
+
+int main(){
+    string filename ="tree09";
+    Mat img = imread(filename + ".png", IMREAD_GRAYSCALE);
+
+    for(int i = 0; i < img.rows; ++i){
+        for(int j = 0; j < img.cols; ++j){
+            if(img.at<uchar>(i, j) != 255){
+                img.at<uchar>(i, j) = 0;
+            }
+        }
+    }
+    imwrite("results/" + filename + ".png", img);
+}
