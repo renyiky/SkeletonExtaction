6d14a2fbb1d367c04ab6772accfe5b7d3925ba30
Ren Yi <renyiky@qq.com>
2020-10-23 17:27:58 +0800

    test on DM alg

--- dataGenerator/pruned/inverter.cpp
+++ dataGenerator/pruned/inverter.cpp
@@ -8,12 +8,18 @@ using namespace cv;
 
 int main(int argc, char *argv[]){
     string filename = argv[1];
-    Mat img = imread(filename + ".png", IMREAD_GRAYSCALE);
+    Mat img = imread(filename + ".png", IMREAD_COLOR);
 
     for(int i = 0; i < img.rows; ++i){
         for(int j = 0; j < img.cols; ++j){
-            if(img.at<uchar>(i, j) != 255){
-                img.at<uchar>(i, j) = 0;
+            if(img.at<uchar>(i, j, 0) == 47 && img.at<uchar>(i, j, 1) == 47 && img.at<uchar>(i, j, 2) == 47){
+                img.at<uchar>(i, j, 0) = 255;
+                img.at<uchar>(i, j, 1) = 255;
+                img.at<uchar>(i, j, 2) = 255;
+            }else if(img.at<uchar>(i, j, 0) == 0 && img.at<uchar>(i, j, 1) == 0 && img.at<uchar>(i, j, 0) == 0){
+                img.at<uchar>(i, j, 0) = 255;
+                img.at<uchar>(i, j, 1) = 255;
+                img.at<uchar>(i, j, 2) = 255;
             }
         }
     }
